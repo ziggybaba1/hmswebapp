9b796cf8cf11e951fe0c00c306a9931b3d39c2b0
Ziggybaba1 <seyiadejugbagbe@gmail.com>
2021-12-20 02:03:14 +0100

    data removed

--- src/env.js
+++
@@ -1,4 +0,0 @@
-const appConfig = window.globalConfig || { siteName: process.env.REACT_APP_SITENAME}
-export const baseUrl = appConfig.server+'/api';
-export const baseRoot = appConfig.server;
-export const secretkey='';
